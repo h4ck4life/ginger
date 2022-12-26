66ade58bff1a2879f985894132a7cff2d9102c9d
Alif <alifaziz@gmail.com>
2022-12-26 11:39:58 +0800

    Add some meta data

--- main.go
+++ main.go
@@ -22,7 +22,11 @@ func main() {
 	models.ConnectDatabase()
 
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"data": "go /quotes to get a random quote"})
+		c.JSON(http.StatusOK, gin.H{
+			"data":   "get /random quote from over 499,500 records",
+			"source": "https://github.com/h4ck4life/ginger",
+			"author": "@h4ck4life",
+		})
 	})
 
 	r.GET("/random", controllers.FindQuotes)
