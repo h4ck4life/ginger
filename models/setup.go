8731d25daaa0796181f57d07ed4700a1788f8f50
Alif <alifaziz@gmail.com>
2024-01-12 11:51:26 +0800

    Add database connection settings

--- models/setup.go
+++ models/setup.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"github.com/uptrace/opentelemetry-go-extra/otelgorm"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -15,6 +17,23 @@ func ConnectDatabase() {
 	if err != nil {
 		panic("Failed to connect to database!")
 	}
+
+	// Get generic database object sql.DB to use its functions
+	sqlDB, err := database.DB()
+	if err != nil {
+		panic("failed to get database")
+	}
+
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	sqlDB.SetMaxIdleConns(10)
+
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	sqlDB.SetMaxOpenConns(100)
+
+	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	//defer sqlDB.Close()
 
 	/*
 		err = database.AutoMigrate(&Quote{})
